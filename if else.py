a3e0a8a73aa8d8478e7165e895e0cc198c57b237
varun3834 <139316140+varun3834@users.noreply.github.com>
2023-07-30 18:55:19 +0530

    Update if else.py

--- if else.py
+++ if else.py
@@ -6,17 +6,6 @@ import sys
 
 
 n = int(input().strip())
- 
-# l1=[2,4]
-# l2=[6,8,10,12,14,16,18,20]
-# if n in l1:
-#     print("Not Weird")
-# elif n in l2:
-#     print("Weird")
-# elif n>=20:
-#     print("Weird")
-# else:
-#     print("Not Weird")
 if n%2!=0:
    print("Weird")
 elif n>=2 and n<=5:
